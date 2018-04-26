49abccdc992f9b13e3adf2fb4beb1d0bc3db7315
AryaKnight <mbratton@bsu.edu>
2018-04-26 17:59:29 -0400

    // Change layout

--- src/edu/bsu/cs345/Main.java
+++ src/edu/bsu/cs345/Main.java
@@ -12,8 +12,17 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MoundsTrailExplorer.fxml"));
         primaryStage.setTitle("Mounds State Park Trail Explorer");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.resizableProperty();
+        primaryStage.setResizable(false);
+
+
+
+        primaryStage.setMinHeight(600);
+        primaryStage.setMinWidth(1000);
         primaryStage.show();
+        System.out.println(primaryStage.getWidth());
+        System.out.println(primaryStage.getHeight());
     }
 
 
